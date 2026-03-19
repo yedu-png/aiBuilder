21ccf4848243e509affe37d16323804f4df2dee0
yedu <127228978+yedu-png@users.noreply.github.com>
2026-03-19 14:08:22 +0530

    feat: initial commit from AppForge

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+<!-- Header.jsx -->
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+    </nav>
+  );
+}
+
+export default Header;
