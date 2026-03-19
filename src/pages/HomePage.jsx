84de48e0b2abc5b46774ec8851056039b64f0934
yedu <127228978+yedu-png@users.noreply.github.com>
2026-03-19 14:08:25 +0530

    feat: initial commit from AppForge

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+<!-- HomePage.jsx -->
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Smart Library Manager</h1>
+    </div>
+  );
+}
+
+export default HomePage;
